df51065013e40e32aadd269c0a54c0b37c3b8015
unknown <jolantua@gmail.com>
2021-02-20 00:12:42 +0100

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
+import React, {Component} from 'react'
 import Form from './components/form/Form'
 import Result from './components/result/Result';
 import './App.css';
 
 const apiKey = '93ff829608b842116f66ee2c07d4b476'
-function App() {
-  return (
-    <div className="App">
-     Weather App
-     <Form/>
-     <Result/>
-    </div>
-  );
+// const API = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+export default class App extends Component {
+  state = {
+    value: '',
+    date: '',
+    city: '',
+    sunrise: '',
+    sunset: '',
+    temp: '',
+    pressure: '',
+    wind: '',
+    err: ''
+  }
+  handleInputChange = e => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+  handleCitySubmit = e => {
+    e.preventDefault()
+    const API = `api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${apiKey}&units=metric`
+
+    fetch(API)
+      .then(response => {
+        if(response.ok) {
+          return response
+        }
+        throw Error("Failed")
+      })
+      // .then(response => response.json())
+      .then( response => {
+
+        console.log(response)
+      }
+      
+        
+      )
+      .catch(error => console.log(error))
+  }
+  render() {
+    return (
+      <div className='container mt-5'>
+        <Form 
+          value={this.state.value} 
+          change={this.handleInputChange}
+          submit={this.handleCitySubmit}/>
+        <Result/>
+      </div>
+    )
+  }
 }
 
-export default App;
--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,7 +1,15 @@
-const Form = () => {
+const Form = ({value, change, submit}) => {
     return (
-        <form>
-            <input type='text'/>
+        <form 
+            className='input-group mb-3 form-width'
+            onSubmit={submit}> 
+            <input 
+                className='form-control'
+                type='text' 
+                value={value}
+                placeholder='enter city'
+                onChange={change}/>
+            <button className='btn btn-dark btn-outline-secondary'>search</button>
         </form>
     ) 
 }
